f389e3da0c9a70001fb716061c85074c7c1f805b
BrydeRov <andresjavieroksi@gmail.com>
2022-06-01 13:39:18 -0500

    Ya subelo asi CTM

--- Metodos.py
+++ Metodos.py
@@ -1,29 +1,29 @@
 
+from matplotlib import pyplot as plt
 from functions import *
-
 titulo()
 
 
 
 def Euler(t,y,h,N):
-    temp=0
     print("Euler")
     print("\nAproximada\n")
     y_exacta = y
     t_exacta = t
 
-    for k in range(N):
-        print('y(',round(t, 2),')=', round(y, 4))
+    for k in range(N+1):
+        print(k+1,")\t",'y(',round(t, 4),')=', round(y, 4))
+        print()
         funcion_exacta = f_ex(t_exacta)
-        ErrorAbsoluto = abs(y - funcion_exacta)
-        ErrorRelativo = 100*(ErrorAbsoluto/(abs(funcion_exacta)))
-        y = y+h*f(t,y)
-        t = t+h        
+        ErrorAbsoluto = abs(funcion_exacta - y)
+        ErrorRelativo = ((ErrorAbsoluto) / (abs(funcion_exacta)))              
         print(
             "\t\t\t==========", '\t\t\ty(',round(t_exacta, 2),')=',round(funcion_exacta, 4),
             "\t\t\t==========\t\t", ErrorAbsoluto,
             "\t\t\t==========\t\t\t", ErrorRelativo
             )
+        y = y+h*f(t,y)
+        t = t+h  
         t_exacta = t_exacta + h
         
          
@@ -35,8 +35,8 @@ def Heun(t,y,h,N):
     for k in range(N):
         print('y(',round(t, 2),')=', round(y, 4))
         funcion_exacta = f_ex(t_exacta)
-        ErrorAbsoluto = abs(y - funcion_exacta)
-        ErrorRelativo = 100*(ErrorAbsoluto/(abs(funcion_exacta)))
+        ErrorAbsoluto = abs(funcion_exacta - y)
+        ErrorRelativo = (ErrorAbsoluto / (abs(funcion_exacta)) )
         y0 = y+h*f(t,y)
         y = y+(h/2)*(f(t,y)+f(t, y0))
         t = t+h        
